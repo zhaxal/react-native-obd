06c099e74b496b404b77732cde7df87316516164
zhaxal <zhaxa65@gmail.com>
2022-04-17 22:09:39 +0600

    test

--- android/src/main/java/com/reactnativeobd/services/BluetoothService.java
+++ android/src/main/java/com/reactnativeobd/services/BluetoothService.java
@@ -18,7 +18,6 @@ import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.github.pires.obd.commands.ObdCommand;
-import com.github.pires.obd.commands.engine.RPMCommand;
 import com.google.gson.Gson;
 import com.reactnativeobd.models.Device;
 import com.reactnativeobd.models.ObdData;
@@ -43,24 +42,20 @@ public class BluetoothService {
   private final Runnable dataRunnable = new Runnable() {
     public void run() {
       try {
-//        Gson g = new Gson();
-//        WritableMap data = new WritableNativeMap();
-//
-//        ArrayList<ObdCommand> commands = ObdService.getCommands();
-//        for (int i = 0; i < commands.size(); i++) {
-//          ObdCommand command = commands.get(i);
-//          command.run(socket.getInputStream(), socket.getOutputStream());
-//          JSONObject jo = new JSONObject(g.toJson(new ObdData(command.getName(), command.getFormattedResult())));
-//          WritableMap wm = convertJsonToMap(jo);
-//          data.putMap(ObdService.LookUpCommand(command.getName()), wm);
-//        }
+        Gson g = new Gson();
+        WritableMap data = new WritableNativeMap();
 
-        RPMCommand rpmCommand = new RPMCommand();
-        rpmCommand.run(socket.getInputStream(), socket.getOutputStream());
+        ArrayList<ObdCommand> commands = ObdService.getCommands();
+        for (int i = 0; i < commands.size(); i++) {
+          ObdCommand command = commands.get(i);
+          command.run(socket.getInputStream(), socket.getOutputStream());
+          JSONObject jo = new JSONObject(g.toJson(new ObdData(command.getName(), command.getFormattedResult())));
+          WritableMap wm = convertJsonToMap(jo);
+          data.putMap(ObdService.LookUpCommand(command.getName()), wm);
+        }
 
-
-        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("liveData", rpmCommand.getFormattedResult());
-      } catch (IOException | InterruptedException e) {
+        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("liveData", data);
+      } catch (IOException | InterruptedException | JSONException e) {
         e.printStackTrace();
       }
 
--- android/src/main/java/com/reactnativeobd/services/ObdService.java
+++ android/src/main/java/com/reactnativeobd/services/ObdService.java
@@ -48,50 +48,50 @@ public class ObdService {
     ArrayList<ObdCommand> cmds = new ArrayList<>();
 
     // Control
-    cmds.add(new ModuleVoltageCommand());
-    cmds.add(new EquivalentRatioCommand());
-    cmds.add(new DistanceMILOnCommand());
-    cmds.add(new DtcNumberCommand());
-    cmds.add(new TimingAdvanceCommand());
-    cmds.add(new TroubleCodesCommand());
-    cmds.add(new VinCommand());
+//    cmds.add(new ModuleVoltageCommand());
+//    cmds.add(new EquivalentRatioCommand());
+//    cmds.add(new DistanceMILOnCommand());
+//    cmds.add(new DtcNumberCommand());
+//    cmds.add(new TimingAdvanceCommand());
+//    cmds.add(new TroubleCodesCommand());
+//    cmds.add(new VinCommand());
 
     // Engine
-    cmds.add(new LoadCommand());
+//    cmds.add(new LoadCommand());
     cmds.add(new RPMCommand());
-    cmds.add(new RuntimeCommand());
-    cmds.add(new MassAirFlowCommand());
-    cmds.add(new ThrottlePositionCommand());
-
-    // Fuel
-    cmds.add(new FindFuelTypeCommand());
-    cmds.add(new ConsumptionRateCommand());
-    // cmds.add(new AverageFuelEconomyObdCommand());
-    //cmds.add(new FuelEconomyCommand());
-    cmds.add(new FuelLevelCommand());
-    // cmds.add(new FuelEconomyMAPObdCommand());
-    // cmds.add(new FuelEconomyCommandedMAPObdCommand());
-    cmds.add(new FuelTrimCommand(FuelTrim.LONG_TERM_BANK_1));
-    cmds.add(new FuelTrimCommand(FuelTrim.LONG_TERM_BANK_2));
-    cmds.add(new FuelTrimCommand(FuelTrim.SHORT_TERM_BANK_1));
-    cmds.add(new FuelTrimCommand(FuelTrim.SHORT_TERM_BANK_2));
-    cmds.add(new AirFuelRatioCommand());
-    cmds.add(new WidebandAirFuelRatioCommand());
-    cmds.add(new OilTempCommand());
-
-    // Pressure
-    cmds.add(new BarometricPressureCommand());
-    cmds.add(new FuelPressureCommand());
-    cmds.add(new FuelRailPressureCommand());
-    cmds.add(new IntakeManifoldPressureCommand());
-
-    // Temperature
-    cmds.add(new AirIntakeTemperatureCommand());
-    cmds.add(new AmbientAirTemperatureCommand());
-    cmds.add(new EngineCoolantTemperatureCommand());
-
-    // Misc
-    cmds.add(new SpeedCommand());
+//    cmds.add(new RuntimeCommand());
+//    cmds.add(new MassAirFlowCommand());
+//    cmds.add(new ThrottlePositionCommand());
+//
+//    // Fuel
+//    cmds.add(new FindFuelTypeCommand());
+//    cmds.add(new ConsumptionRateCommand());
+//    // cmds.add(new AverageFuelEconomyObdCommand());
+//    //cmds.add(new FuelEconomyCommand());
+//    cmds.add(new FuelLevelCommand());
+//    // cmds.add(new FuelEconomyMAPObdCommand());
+//    // cmds.add(new FuelEconomyCommandedMAPObdCommand());
+//    cmds.add(new FuelTrimCommand(FuelTrim.LONG_TERM_BANK_1));
+//    cmds.add(new FuelTrimCommand(FuelTrim.LONG_TERM_BANK_2));
+//    cmds.add(new FuelTrimCommand(FuelTrim.SHORT_TERM_BANK_1));
+//    cmds.add(new FuelTrimCommand(FuelTrim.SHORT_TERM_BANK_2));
+//    cmds.add(new AirFuelRatioCommand());
+//    cmds.add(new WidebandAirFuelRatioCommand());
+//    cmds.add(new OilTempCommand());
+//
+//    // Pressure
+//    cmds.add(new BarometricPressureCommand());
+//    cmds.add(new FuelPressureCommand());
+//    cmds.add(new FuelRailPressureCommand());
+//    cmds.add(new IntakeManifoldPressureCommand());
+//
+//    // Temperature
+//    cmds.add(new AirIntakeTemperatureCommand());
+//    cmds.add(new AmbientAirTemperatureCommand());
+//    cmds.add(new EngineCoolantTemperatureCommand());
+//
+//    // Misc
+//    cmds.add(new SpeedCommand());
 
 
     return cmds;
